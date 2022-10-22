49720945b672645176999a03a5bd27a4d01c8090
blue32a <blue32a.jp@gmail.com>
2022-10-22 23:59:41 +0900

    第4章 意図を語るテスト (Privacy)

--- TODO.md
+++ TODO.md
@@ -1,7 +1,9 @@
 - [ ] $5 + 10 CHF = $10 （レートが 2:1 の場合）
 - [x] $5 * 2 = $10
-- [ ] amount を private にする
+- [x] amount を private にする
 - [x] Dollar の副作用どうする？
 - [ ] Money の丸め処理どうする？
 - [x] equals()
 - [ ] hashCode()
+- [ ] nullとの等価性比較
+- [ ] 他のオブジェクトとの等価性比較
---
+++ src/ch04-privacy/money/dollar.go
@@ -0,0 +1,17 @@
+package money
+
+type Dollar struct {
+    amount int64
+}
+
+func NewDollar(amount int64) Dollar {
+    return Dollar{amount: amount}
+}
+
+func (d *Dollar) Times(multiplier int64) Dollar {
+    return Dollar{amount: d.amount * multiplier}
+}
+
+func (d *Dollar) Equals(dollar Dollar) bool {
+    return d.amount == dollar.amount
+}
