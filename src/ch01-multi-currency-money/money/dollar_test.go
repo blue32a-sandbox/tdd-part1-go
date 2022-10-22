18ed576cd5bc78e265ab3c7603b836cb9c249e0f
blue32a <blue32a.jp@gmail.com>
2022-10-22 23:56:18 +0900

    refactor: typo `actoual` -> `actual`

--- src/ch01-multi-currency-money/money/dollar_test.go
+++ src/ch01-multi-currency-money/money/dollar_test.go
@@ -6,6 +6,6 @@ func TestMultiplication(t *testing.T) {
     five := Dollar{Amount: 5}
     five.Times(2)
     if 10 != five.Amount {
-        t.Fatalf(`amount is not equal. expect: 10, actoual: %d`, five.Amount)
+        t.Fatalf(`amount is not equal. expect: 10, actual: %d`, five.Amount)
     }
 }
--- src/ch03-equality-for-all/money/dollar_test.go
+++ src/ch03-equality-for-all/money/dollar_test.go
@@ -6,11 +6,11 @@ func TestMultiplication(t *testing.T) {
     five := Dollar{Amount: 5}
     var product Dollar = five.Times(2)
     if 10 != product.Amount {
-        t.Fatalf(`amount is not equal. expect: 10, actoual: %d`, product.Amount)
+        t.Fatalf(`amount is not equal. expect: 10, actual: %d`, product.Amount)
     }
     product = five.Times(3)
     if 15 != product.Amount {
-        t.Fatalf(`amount is not equal. expect: 15, actoual: %d`, product.Amount)
+        t.Fatalf(`amount is not equal. expect: 15, actual: %d`, product.Amount)
     }
 }
 
