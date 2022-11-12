6016ca2ccc4d5584862546984fe858f48f2f2627
blue32a <blue32a.jp@gmail.com>
2022-11-12 16:53:28 +0900

    第12章 設計とメタファー (Addition, Finally)

--- TODO.md
+++ TODO.md
@@ -1,17 +1,6 @@
 - [ ] $5 + 10 CHF = $10 （レートが 2:1 の場合）
-- [x] $5 * 2 = $10
-- [x] amount を private にする
-- [x] Dollar の副作用どうする？
+- [ ] $5 + $5 = $10
 - [ ] Money の丸め処理どうする？
-- [x] equals()
 - [ ] hashCode()
 - [ ] nullとの等価性比較
 - [ ] 他のオブジェクトとの等価性比較
-- [x] 5CHF * 2 = 10CHF
-- [x] Dollar と Franc の重複
-- [x] equals の一般化
-- [x] times の一般化
-- [x] Franc と Dollar を比較する
-- [x] 通貨の概念
-- [x] 改めて equals の一般化
-- [x] TestFrancMultiplication を削除する？
---
+++ src/ch12-addition-finally/money/money.go
@@ -0,0 +1,37 @@
+package money
+
+type Expression interface {}
+
+type Money struct {
+    amount int64
+    currency string
+}
+
+func (m Money) Amount() int64 {
+    return m.amount
+}
+
+func (m Money) Currency() string {
+    return m.currency
+}
+
+func (m Money) Equals(money Money) bool {
+    return m.amount == money.amount &&
+        m.currency == money.currency
+}
+
+func (m Money) Times(multiplier int64) Money {
+    return Money{m.amount * multiplier, m.currency}
+}
+
+func (m Money) Plus(addend Money) Expression {
+    return Money{m.amount + addend.amount, m.currency}
+}
+
+func NewDollar(amount int64) Money {
+    return Money{amount, "USD"}
+}
+
+func NewFranc(amount int64) Money {
+    return Money{amount, "CHF"}
+}
